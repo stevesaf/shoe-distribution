62265e319cd804eb4831cfe64fa5980b2e6d1a31
Steve Saf <stephensaf@gmail.com>
2017-04-14 16:41:33 +0800

    can add brands to stores and vice versa

--- app.rb
+++ app.rb
@@ -7,3 +7,55 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get("/") do
   erb(:index)
 end
+
+get("/brands") do
+  @brands = Brand.all()
+  erb(:brands)
+end
+
+get("/brands/:id") do
+  @brand = Brand.find(params.fetch("id").to_i())
+  @stores = Store.all()
+  erb(:brand)
+end
+
+get("/stores/:id") do
+  @store = Store.find(params.fetch("id").to_i())
+  @brands = Brand.all()
+  erb(:store)
+end
+
+get("/stores") do
+  @stores = Store.all()
+  erb(:stores)
+end
+
+post("/stores") do
+  name = params.fetch("name")
+  Store.create({:name => name})
+  redirect("/stores")
+end
+
+post("/brands") do
+  name = params.fetch("name")
+  Brand.create({:name => name})
+  redirect("/brands")
+end
+
+patch("/brands/:id") do
+  brand_id = params.fetch("id").to_i()
+  @brand = Brand.find(brand_id)
+  store_ids = params.fetch("store_ids") #this is coming from our store_ids[] we created in the name for the checkboxes
+  @brand.update({:store_ids => store_ids})
+  @stores = Store.all()
+  erb(:brand)
+end
+
+patch("/stores/:id") do
+  store_id = params.fetch("id").to_i()
+  @store = Store.find(store_id)
+  brand_ids = params.fetch("brand_ids") #this is coming from our store_ids[] we created in the name for the checkboxes
+  @store.update({:brand_ids => brand_ids})
+  @brands = Brand.all()
+  erb(:store)
+end
